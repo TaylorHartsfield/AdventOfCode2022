8f962c77a35b92e0aa589bf51668adfb833f4afa
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-12-04 10:51:53 -0700

    Day4 Part2 Correct

--- Day4/day4.py
+++ Day4/day4.py
@@ -18,12 +18,21 @@ for sections in pairs:
     elf_1_sections = split_sections[0].split('-')
     elf_2_sections = split_sections[1].split('-')
     
-    min_section = min(int(elf_1_sections[0]), int(elf_2_sections[0]))
-    max_section = max(int(elf_1_sections[1]), int(elf_2_sections[1]))
+    # min_section = min(int(elf_1_sections[0]), int(elf_2_sections[0]))
+    # max_section = max(int(elf_1_sections[1]), int(elf_2_sections[1]))
 
    
-    if (min_section == int(elf_1_sections[0]) and max_section == int(elf_1_sections[1])) or\
-    (min_section == int(elf_2_sections[0]) and max_section == int(elf_2_sections[1])):
-        OVERLAP_SECTIONS += 1
+    # if (min_section == int(elf_1_sections[0]) and max_section == int(elf_1_sections[1])) or\
+    # (min_section == int(elf_2_sections[0]) and max_section == int(elf_2_sections[1])):
+    #     OVERLAP_SECTIONS += 1
 
 
+# PART 2 CORRECT ⭐️ (Comment out line 21-27 to run part 2 and vice versa)
+
+    full_range = range(int(elf_1_sections[0]), int(elf_1_sections[1])+1)
+    for val in range(int(elf_2_sections[0]), int(elf_2_sections[1])+1):
+        if val in full_range:
+            OVERLAP_SECTIONS+=1
+            break
+  
+    
