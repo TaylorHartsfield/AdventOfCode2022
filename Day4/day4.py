b714ca3dff4f91a9de4f62a255768fb40a6ba95f
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-12-04 10:39:39 -0700

    Day4 Part1 Correct

---
+++ Day4/day4.py
@@ -0,0 +1,29 @@
+data = open('input.txt', 'r')
+pairs = data.read().split('\n')
+
+test = ['2-4,6-8',
+        '2-3,4-5',
+        '5-7,7-9',
+        '2-8,3-7',
+        '6-6,4-6',
+        '2-6,4-8']
+
+
+#PART 1 CORRECT ⭐️
+OVERLAP_SECTIONS = 0
+
+for sections in pairs:
+    sections.split(',')
+    split_sections = sections.split(',')
+    elf_1_sections = split_sections[0].split('-')
+    elf_2_sections = split_sections[1].split('-')
+    
+    min_section = min(int(elf_1_sections[0]), int(elf_2_sections[0]))
+    max_section = max(int(elf_1_sections[1]), int(elf_2_sections[1]))
+
+   
+    if (min_section == int(elf_1_sections[0]) and max_section == int(elf_1_sections[1])) or\
+    (min_section == int(elf_2_sections[0]) and max_section == int(elf_2_sections[1])):
+        OVERLAP_SECTIONS += 1
+
+
