d9ffc5ddbdcbb9f24ce43e1d88a2187265f20fbf
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-12-04 10:17:45 -0700

    Day3 Part2 Correct

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,6 +1,12 @@
 data = open('input.txt', 'r')
 rucksacks = data.read().split('\n')
 
+test = ['vJrwpWtwJgWrhcsFMMfFFhFp',
+        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+        'PmmdzqPrVvPwwTWBwg',
+        'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+        'ttgJtRGJQctTZtZT',
+        'CrZsJsPPZsGzwwsLwLmpwMDw']
 
 # PART 1 CORRECT ⭐️
 LOWER = "abcdefghijklmnopqrstuvwxyz"
@@ -24,14 +30,36 @@ def make_priority_dict(LOWER, UPPER):
     
 priority_values_dict = make_priority_dict(LOWER, UPPER)
 
-for rucksack in rucksacks:
-    split_sack = len(rucksack)//2
-    compartment_1 = set(rucksack[:split_sack])
-    compartment_2 = rucksack[split_sack:]
+# for rucksack in rucksacks:
+#     split_sack = len(rucksack)//2
+#     compartment_1 = set(rucksack[:split_sack])
+#     compartment_2 = rucksack[split_sack:]
 
-    for item in compartment_2:
-        if item in compartment_1:
-            TOTAL += priority_values_dict[item]
-            break
+#     for item in compartment_2:
+#         if item in compartment_1:
+#             TOTAL += priority_values_dict[item]
+#             break
     
 
+# PART 2 CORRECT ⭐️
+
+def make_groups(rucksacks):
+    GROUPS = []
+    group_beg = 0
+    group_end = 2
+    while group_end < len(rucksacks):
+        GROUPS.append(rucksacks[group_beg:group_end+1])
+        group_beg = group_end+1
+        group_end += 3
+    
+    return GROUPS
+
+elf_groups = make_groups(rucksacks)
+
+for group in elf_groups:
+    sack_1 = set(group[0])
+    sack_2 = set(group[1])
+    for item in group[2]:
+        if item in sack_1 and item in sack_2:
+            TOTAL += priority_values_dict[item]
+            break
