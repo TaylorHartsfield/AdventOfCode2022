67ba6e259af0d7301675893b1ca8618b5ac1ba96
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-12-07 11:19:51 -0700

    day6 part 1 correct

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,1 +1,16 @@
 data = open('input.txt', 'r')
+characters = data.read().split('\n')
+
+#PART 1 CORRECT ⭐️
+marker = 0
+
+for i in range(len(characters[0])-1):
+    check = set(characters[0][i:i+4])
+    if len(check) == 4 and marker >= 4:
+        marker+=4
+        break
+    else:
+        marker += 1
+
+    
+print(marker)
